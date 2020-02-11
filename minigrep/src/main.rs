f8e8c2009df13241ffd022d8ba3d65ad9b7c350f
朱杰 <zhujie@zhujiedeMacBook-Pro.local>
2020-02-11 19:07:34 +0800

    错误信息输出至标准错误输出

--- minigrep/src/main.rs
+++ minigrep/src/main.rs
@@ -8,7 +8,7 @@ fn main() {
     //let query = &args[1];
     //let filename = &args[2];
     let config = Config::new(&args).unwrap_or_else(|err|{ //一个匿名函数
-            println!("Problem parsing arguments: {}", err);
+            eprintln!("Problem parsing arguments: {}", err);
             process::exit(1);//出错时退出
         }
     );
@@ -17,7 +17,7 @@ fn main() {
     println!("In file {}", config.filename);
 
     if let Err(e) = run(config) {
-        println!("Application error: {}", e);
+        eprintln!("Application error: {}", e);//输出至标准错误输出
         process::exit(1);
     }
 }

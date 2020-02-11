4bd0643ffd70c7455fbd252d715c4441f7381b1c
朱杰 <zhujie@zhujiedeMacBook-Pro.local>
2020-02-11 18:57:24 +0800

    添加单元测试和字符串搜索方法

--- minigrep/src/lib.rs
+++ minigrep/src/lib.rs
@@ -1,5 +1,6 @@
 use std::fs;
 use std::error::Error;
+
 pub struct Config{
     pub query:String,
     pub filename:String,
@@ -24,7 +25,40 @@ pub fn run(config: Config) -> Result<(), Box<dyn Error>>{
     let contents = fs::read_to_string(config.filename)
         .expect("Something went wrong reading the file");
 
-    println!("With text:\n{}", contents);
+    //println!("With text:\n{}", contents);
+    for line in search(&config.query,&contents){
+        println!("{}", line);
+    }
 
     Ok(())
+}
+
+pub fn search<'a>(query: &str, contents: &'a str) -> Vec<&'a str> {
+    let mut results = Vec::new();
+
+    for line in contents.lines() {
+        if line.contains(query){
+            results.push(line);
+        }
+    }
+    results
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn one_result() {
+        let query = "duct";
+        let contents = "\
+Rust:
+safe, fast, productive.
+Pick three.";
+
+        assert_eq!(
+            vec!["safe, fast, productive."],
+            search(query, contents)
+        );
+    }
 }

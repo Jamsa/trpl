8c32ba0c5b29554ac87db72d5f433fd441d65d20
zhujie <jiemao@gmail.com>
2022-03-20 23:42:52 +0800

    add macro module

--- adv_funcs/src/main.rs
+++ adv_funcs/src/main.rs
@@ -2,11 +2,11 @@ use std::fmt;
 
 // 1. function pointer
 fn add_one(x: i32) -> i32 {
-  x + 1
+    x + 1
 }
 
 fn do_twice(f: fn(i32) -> i32, x: i32) -> i32 {
-  f(x)+f(x)
+    f(x) + f(x)
 }
 // `fn` is a type rather than a trait, so we specify `fn` as the parameter type directly
 //
@@ -16,26 +16,23 @@ fn do_twice(f: fn(i32) -> i32, x: i32) -> i32 {
 //
 // it's best to write function using trait so the function can accept either functions or closures.
 
-
-
 // 2. exploits detail of tuple structs and tuple struct enum variant.
 #[derive(Debug)]
 enum Status {
-  Value(u32),
-  Stop,
+    Value(u32),
+    Stop,
 }
 
 impl fmt::Display for Status {
-  fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
-    write!(f, "{:?}", self)
-  }
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        write!(f, "{:?}", self)
+    }
 }
-
 
 // 3. returing closures
 // closures are presented by traits.
 // means you can not return it directly.
-// you can return a concret type that implements the trait. 
+// you can return a concret type that implements the trait.
 // closures do not have a concret returnable type.
 // you can not return `dyn Fn` directly, because it's not `Sized`
 fn returns_closure() -> Box<dyn Fn(i32) -> i32> {
@@ -43,26 +40,32 @@ fn returns_closure() -> Box<dyn Fn(i32) -> i32> {
 }
 
 fn main() {
-  let answer = do_twice(add_one, 1);
-  println!("answer is {}",answer);
+    let answer = do_twice(add_one, 1);
+    println!("answer is {}", answer);
 
-  // 1. function pointer or closure trait
-  let list_of_numbers: Vec<i32> = vec![1, 2, 3];
-  // closure
-  let list_of_strings: Vec<String> = list_of_numbers.iter().map(|i| i.to_string()).collect();
-  println!("[{}]",list_of_strings.join(","));
-  // function
-  let list_of_strings:Vec<String> = list_of_numbers.iter().map(ToString::to_string).collect();
-  println!("[{}]",list_of_strings.join(","));
+    // 1. function pointer or closure trait
+    let list_of_numbers: Vec<i32> = vec![1, 2, 3];
+    // closure
+    let list_of_strings: Vec<String> = list_of_numbers.iter().map(|i| i.to_string()).collect();
+    println!("[{}]", list_of_strings.join(","));
+    // function
+    let list_of_strings: Vec<String> = list_of_numbers.iter().map(ToString::to_string).collect();
+    println!("[{}]", list_of_strings.join(","));
 
-  // 2.
-  let x = Status::Stop;
-  println!("{}",x);
-  // init a series of Value
-  let list_of_statuses: Vec<Status> = (0u32..10).map(Status::Value).collect();
-  println!("[{}]",list_of_statuses.iter().map(|i| i.to_string()).collect::<String>());
+    // 2.
+    let x = Status::Stop;
+    println!("{}", x);
+    // init a series of Value
+    let list_of_statuses: Vec<Status> = (0u32..10).map(Status::Value).collect();
+    println!(
+        "[{}]",
+        list_of_statuses
+            .iter()
+            .map(|i| i.to_string())
+            .collect::<String>()
+    );
 
-  // 3. returing closures
-  let clo = returns_closure();
-  println!("closure result = {}", clo(10));
+    // 3. returing closures
+    let clo = returns_closure();
+    println!("closure result = {}", clo(10));
 }
--- macros/src/main.rs
+++ macros/src/main.rs
@@ -1,11 +1,16 @@
 // macros are a way of writing code that writes other code, which is known as metaprogramming.
 // macros are expanded before the compiler interpreters the meaning of the code
+// the macro definitions are more complex than function definitions
+
 
 // 1. three kinds of procedural macros
 // #[derive] macros used on structs and enums
 // attribute like macros that define custom attributes usable on any item
 // function like macros that look like function calls but operate on the token specified as their argument
-// 
+
+
+// 2. write a custom `derive` macro
+
 
 fn main() {
     println!("Hello, world!");
